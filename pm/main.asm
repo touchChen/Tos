a71326f4d64acad723ffea5be99ebc57221fcaac
touchChen <rstyh.t@gmail.com>
2019-10-15 16:48:33 +0800

    improve the main.asm

--- pm/main.asm
+++ pm/main.asm
@@ -15,7 +15,7 @@ ProcFoo		equ	00401000h
 ProcBar		equ	00501000h
 
 ProcPagingDemo	equ	00301000h
-
+	
 
 org	0100h
 	jmp	LABEL_BEGIN
@@ -23,18 +23,27 @@ org	0100h
 [SECTION .gdt]
 ; GDT
 ;                                         段基址,       段界限     , 属性
-LABEL_GDT:		Descriptor	       0,                 0, 0			; 空描述符
-LABEL_DESC_NORMAL:	Descriptor	       0,            0ffffh, DA_DRW		; Normal 描述符
-LABEL_DESC_PAGE_DIR:	Descriptor   PageDirBase,              4095, DA_DRW		; Page Directory
-LABEL_DESC_PAGE_TBL:	Descriptor   PageTblBase,      4096 * 8 - 1, DA_DRW		; Page Tables
-LABEL_DESC_CODE32:	Descriptor	       0,  SegCode32Len - 1, DA_CR + DA_32	; 非一致代码段, 32
-LABEL_DESC_CODE16:	Descriptor	       0,            0ffffh, DA_C		; 非一致代码段, 16
-LABEL_DESC_DATA:	Descriptor	       0,	DataLen - 1, DA_DRW		; Data
-LABEL_DESC_STACK:	Descriptor	       0,        TopOfStack, DA_DRWA + DA_32	; Stack, 32 位
-LABEL_DESC_VIDEO:	Descriptor	 0B8000h,            0ffffh, DA_DRW		; 显存首地址
+LABEL_GDT:		Descriptor	       0,                 0, 0			           ; 空描述符
+LABEL_DESC_NORMAL:	Descriptor	       0,            0ffffh, DA_DRW		           ; Normal 描述符
+LABEL_DESC_PAGE_DIR:	Descriptor   PageDirBase,              4095, DA_DRW		           ; Page Directory
+LABEL_DESC_PAGE_TBL:	Descriptor   PageTblBase,      4096 * 8 - 1, DA_DRW		           ; Page Tables
+LABEL_DESC_CODE32:	Descriptor	       0,  SegCode32Len - 1, DA_CR + DA_32	           ; 非一致代码段, 32
+LABEL_DESC_CODE16:	Descriptor	       0,            0ffffh, DA_C		           ; 非一致代码段, 16
+LABEL_DESC_DATA:	Descriptor	       0,	DataLen - 1, DA_DRW		           ; Data
+LABEL_DESC_STACK:	Descriptor	       0,        TopOfStack, DA_DRWA + DA_32	           ; Stack, 32 位
+LABEL_DESC_VIDEO:	Descriptor	 0B8000h,            0ffffh, DA_DRW + DA_DPL3	           ; 显存首地址
 
-LABEL_DESC_FLAT_C:      Descriptor             0,           0fffffh, DA_CR|DA_32|DA_LIMIT_4K; 0~4G
-LABEL_DESC_FLAT_RW:     Descriptor             0,           0fffffh, DA_DRW|DA_LIMIT_4K     ; 0~4G
+LABEL_DESC_FLAT_C:      Descriptor             0,           0fffffh, DA_CR|DA_32|DA_LIMIT_4K       ; 0~4G
+LABEL_DESC_FLAT_RW:     Descriptor             0,           0fffffh, DA_DRW|DA_LIMIT_4K            ; 0~4G
+
+; 新增加
+LABEL_DESC_LDT:         Descriptor             0,         LDTLen - 1, DA_LDT	                   ; LDT
+LABEL_DESC_CODE_DEST:   Descriptor             0,   SegCodeDestLen-1, DA_C + DA_32                 ; 非一致代码段,32
+LABEL_DESC_CODE_RING3:  Descriptor             0,  SegCodeRing3Len-1, DA_C + DA_32 + DA_DPL3
+LABEL_DESC_STACK3:      Descriptor             0,        TopOfStack3, DA_DRWA + DA_32 + DA_DPL3
+LABEL_DESC_TSS:         Descriptor             0,           TSSLen-1, DA_386TSS	                   ;TSS
+; 门                              目标选择子,           偏移,  DCount,    属性
+LABEL_CALL_GATE:        Gate      SelectorCodeDest,       0,       0,    DA_386CGate + DA_DPL3
 ; GDT 结束
 
 GdtLen		equ	$ - LABEL_GDT	; GDT长度
@@ -54,7 +63,39 @@ SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
 SelectorFlatRW		equ	LABEL_DESC_FLAT_RW	- LABEL_GDT
 SelectorFlatC		equ	LABEL_DESC_FLAT_C	- LABEL_GDT
 
+SelectorLDT		equ	LABEL_DESC_LDT		- LABEL_GDT
+SelectorCodeDest	equ	LABEL_DESC_CODE_DEST	- LABEL_GDT
+SelectorCodeRing3	equ	LABEL_DESC_CODE_RING3	- LABEL_GDT + SA_RPL3
+SelectorCallGate	equ	LABEL_CALL_GATE 	- LABEL_GDT + SA_RPL3
+SelectorStack3		equ	LABEL_DESC_STACK3	- LABEL_GDT + SA_RPL3
+SelectorTSS		equ	LABEL_DESC_TSS		- LABEL_GDT
+
+
 ; END of [SECTION .gdt]
+
+
+; IDT
+[SECTION .idt]
+ALIGN	32
+[BITS	32]
+LABEL_IDT:
+; 门                        目标选择子,            偏移, DCount, 属性   SpuriousHandler
+%rep 32
+		Gate	SelectorCode32, SpuriousHandler,      0, DA_386IGate
+%endrep
+.020h:		Gate	SelectorCode32,    ClockHandler,      0, DA_386IGate
+%rep 95
+		Gate	SelectorCode32, SpuriousHandler,      0, DA_386IGate
+%endrep
+.080h:		Gate	SelectorCode32,  UserIntHandler,      0, DA_386IGate
+
+
+IdtLen		equ	$ - LABEL_IDT
+IdtPtr		dw	IdtLen - 1	; 段界限
+		dd	0		; 基地址
+; END of [SECTION .idt]
+
+
 
 [SECTION .data1]	 ; 数据段
 ALIGN	32
@@ -123,26 +164,52 @@ TopOfStack	equ	$ - LABEL_STACK - 1
 ; END of [SECTION .gs]
 
 
-; IDT
-[SECTION .idt]
+; 堆栈段ring3
+[SECTION .s3]
 ALIGN	32
 [BITS	32]
-LABEL_IDT:
-; 门                        目标选择子,            偏移, DCount, 属性   SpuriousHandler
-%rep 32
-		Gate	SelectorCode32, SpuriousHandler,      0, DA_386IGate
-%endrep
-.020h:		Gate	SelectorCode32,    ClockHandler,      0, DA_386IGate
-%rep 95
-		Gate	SelectorCode32, SpuriousHandler,      0, DA_386IGate
-%endrep
-.080h:		Gate	SelectorCode32,  UserIntHandler,      0, DA_386IGate
+LABEL_STACK3:
+	times 512 db 0
+TopOfStack3	equ	$ - LABEL_STACK3 - 1
+; END of [SECTION .s3]
 
 
-IdtLen		equ	$ - LABEL_IDT
-IdtPtr		dw	IdtLen - 1	; 段界限
-		dd	0		; 基地址
-; END of [SECTION .idt]
+; TSS ---------------------------------------------------------------------------------------------
+[SECTION .tss]
+ALIGN	32
+[BITS	32]
+LABEL_TSS:
+		DD	0			; Back
+		DD	TopOfStack		; 0 级堆栈
+		DD	SelectorStack		; 
+		DD	0			; 1 级堆栈
+		DD	0			; 
+		DD	0			; 2 级堆栈
+		DD	0			; 
+		DD	0			; CR3
+		DD	0			; EIP
+		DD	0			; EFLAGS
+		DD	0			; EAX
+		DD	0			; ECX
+		DD	0			; EDX
+		DD	0			; EBX
+		DD	0			; ESP
+		DD	0			; EBP
+		DD	0			; ESI
+		DD	0			; EDI
+		DD	0			; ES
+		DD	0			; CS
+		DD	0			; SS
+		DD	0			; DS
+		DD	0			; FS
+		DD	0			; GS
+		DD	0			; LDT
+		DW	0			; 调试陷阱标志
+		DW	$ - LABEL_TSS + 2	; I/O位图基址
+		DB	0ffh			; I/O位图结束标志
+TSSLen		equ	$ - LABEL_TSS
+; TSS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 
 
 [SECTION .s16]
@@ -214,6 +281,81 @@ LABEL_MEM_CHK_OK:
 	shr	eax, 16
 	mov	byte [LABEL_DESC_STACK + 4], al
 	mov	byte [LABEL_DESC_STACK + 7], ah
+
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_STACK3
+	mov	word [LABEL_DESC_STACK3 + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_STACK3 + 4], al
+	mov	byte [LABEL_DESC_STACK3 + 7], ah
+
+        
+        ; 初始化 LDT 在 GDT 中的描述符
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_LDT
+	mov	word [LABEL_DESC_LDT + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_LDT + 4], al
+	mov	byte [LABEL_DESC_LDT + 7], ah
+
+        ; 初始化 LDT 中的描述符
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_CODE_A
+	mov	word [LABEL_LDT_DESC_CODEA + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_LDT_DESC_CODEA + 4], al
+	mov	byte [LABEL_LDT_DESC_CODEA + 7], ah
+
+        xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_CODE_RETURN
+	mov	word [LABEL_LDT_DESC_CODE_RETURN + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_LDT_DESC_CODE_RETURN + 4], al
+	mov	byte [LABEL_LDT_DESC_CODE_RETURN + 7], ah
+
+
+
+        ; 初始化测试调用门的代码段描述符
+	xor	eax, eax
+	mov	ax, cs
+	shl	eax, 4
+	add	eax, LABEL_SEG_CODE_DEST
+	mov	word [LABEL_DESC_CODE_DEST + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE_DEST + 4], al
+	mov	byte [LABEL_DESC_CODE_DEST + 7], ah
+
+
+        ; 初始化Ring3描述符
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_CODE_RING3
+	mov	word [LABEL_DESC_CODE_RING3 + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE_RING3 + 4], al
+	mov	byte [LABEL_DESC_CODE_RING3 + 7], ah
+
+
+	; 初始化 TSS 描述符
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_TSS
+	mov	word [LABEL_DESC_TSS + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_TSS + 4], al
+	mov	byte [LABEL_DESC_TSS + 7], ah
+
+
 
 	; 为加载 GDTR 作准备
 	xor	eax, eax
@@ -305,6 +447,15 @@ LABEL_SEG_CODE32:
 
 	mov	esp, TopOfStack
 
+
+        ;call    SelectorCodeDest:0     
+   
+        ; Load LDT
+	mov	ax, SelectorLDT
+	lldt	ax
+	call	SelectorLDTCodeA:0	; 跳入局部任务
+        ;call	SelectorLDTCodeReturn:0	; 跳入局部任务
+
         
         call	Init8259A
 	int	80h
@@ -320,6 +471,15 @@ LABEL_SEG_CODE32:
 	call    DispMem		        ; 显示内存信息
 	call	SetupPaging		; 启动分页机制
         call    PagingDemo
+
+       
+        mov	ax, SelectorTSS
+	ltr	ax                  ;在任务内发生特权级变换时要切换堆栈，而内层堆栈的指针存放在当前任务的TSS中，所以要设置任务状态段寄存器 TR。
+        push	SelectorStack3
+	push	TopOfStack3
+	push	SelectorCodeRing3
+	push	0
+	retf
 
 	; 到此停止
 	jmp	SelectorCode16:0
@@ -705,4 +865,100 @@ LABEL_GO_BACK_TO_REAL:
 Code16Len	equ	$ - LABEL_SEG_CODE16
 
 ; END of [SECTION .s16code]
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;新增
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; LDT
+[SECTION .ldt]
+ALIGN	32
+LABEL_LDT:
+;                            段基址       段界限      属性
+LABEL_LDT_DESC_CODEA: Descriptor 0, CodeALen - 1, DA_C + DA_32 ; Code, 32 位
+LABEL_LDT_DESC_CODE_RETURN: Descriptor 0, CodeReturnLen - 1, DA_C + DA_32 ; Code, 32 位
+
+LDTLen		equ	$ - LABEL_LDT
+
+; LDT 选择子
+SelectorLDTCodeA	equ	LABEL_LDT_DESC_CODEA	- LABEL_LDT + SA_TIL
+SelectorLDTCodeReturn	equ	LABEL_LDT_DESC_CODE_RETURN	- LABEL_LDT + SA_TIL
+; END of [SECTION .ldt]
+
+
+; CodeA (LDT, 32 位代码段)
+[SECTION .la]
+ALIGN	32
+[BITS	32]
+LABEL_CODE_A:
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+	mov	edi, (80 * 2 + 50) * 2	; 屏幕第 10 行, 第 0 列。
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	al, 'L'
+	mov	[gs:edi], ax
+
+        retf
+
+CodeALen	equ	$ - LABEL_CODE_A
+; END of [SECTION .la]
+
+
+; CodeReturn (LDT, 32 位代码段)
+[SECTION .lr]
+ALIGN	32
+[BITS	32]
+LABEL_CODE_RETURN:
+	jmp	SelectorCode16:0
+
+CodeReturnLen	equ	$ - LABEL_CODE_RETURN
+; END of [SECTION .lr]
+
+
+
+[SECTION .sdest]; 调用门目标段
+ALIGN	32
+[BITS	32]
+LABEL_SEG_CODE_DEST:
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+	mov	edi, (80 * 2 + 55) * 2	; 屏幕第 12 行, 第 0 列。
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	al, 'G'
+	mov	[gs:edi], ax
+
+
+        mov	ax, SelectorLDT
+	lldt	ax
+	call	SelectorLDTCodeReturn:0	; 跳入局部任务
+	;retf
+	
+SegCodeDestLen	equ	$ - LABEL_SEG_CODE_DEST
+; END of [SECTION .sdest]
+
+
+; CodeRing3
+[SECTION .ring3]
+ALIGN	32
+[BITS	32]
+LABEL_CODE_RING3:
+	mov	ax, SelectorVideo
+	mov	gs, ax
+
+	mov	edi, (80 * 2 + 60) * 2
+	mov	ah, 0Ch
+	mov	al, '3'
+	mov	[gs:edi], ax
+
+        call	SelectorCallGate:0
+
+SegCodeRing3Len	equ	$ - LABEL_CODE_RING3
+; END of [SECTION .ring3]
+
+
+
 
