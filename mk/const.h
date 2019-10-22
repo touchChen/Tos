5f0a59b2b9c215ad1037149bbe8291be89b47769
touchen <rstyh.t@gmail.com>
2019-10-23 00:26:59 +0800

    up

---
+++ mk/const.h
@@ -0,0 +1,13 @@
+#ifndef	_TOS_CONST_H_
+#define	_TOS_CONST_H_
+
+
+/* 函数类型 */
+#define	PUBLIC		/* PUBLIC is the opposite of PRIVATE */
+#define	PRIVATE	static	/* PRIVATE x limits the scope of x */
+
+/* GDT 和 IDT 中描述符的个数 */
+#define	GDT_SIZE	128
+
+
+#endif /* _TOS_CONST_H_ */
---
+++ mk/type.h
@@ -0,0 +1,8 @@
+#ifndef	_TOS_TYPE_H_
+#define	_TOS_TYPE_H_
+
+typedef	unsigned int		u32;
+typedef	unsigned short		u16;
+typedef	unsigned char		u8;
+
+#endif /* _TOS_TYPE_H_ */
