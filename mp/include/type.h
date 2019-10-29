7bb027dfbb1d0280109fc2b0114f45df534553e1
touchen <rstyh.t@gmail.com>
2019-10-29 23:09:42 +0800

    add process

---
+++ mp/include/type.h
@@ -0,0 +1,11 @@
+#ifndef	_TOS_TYPE_H_
+#define	_TOS_TYPE_H_
+
+typedef	unsigned int		u32;
+typedef	unsigned short		u16;
+typedef	unsigned char		u8;
+
+ /* typedef  返回类型(*新类型)(参数表)*/
+typedef	void (*int_handler)();   /* typedef  定义了新类型*/
+
+#endif /* _TOS_TYPE_H_ */
---
+++ mp/lib/klib.asm
@@ -0,0 +1,188 @@
+extern  disp_pos
+
+[SECTION .text]
+
+; 导出函数
+global	disp_str
+global  disp_color_str
+global  disp_al
+global  disp_int
+
+global  out_byte
+global  in_byte
+
+
+
+; ========================================================================
+;                  void disp_str(char * info);
+; ========================================================================
+disp_str:
+	push	ebp
+	mov	ebp, esp
+
+	mov	esi, [ebp + 8]	; pszInfo
+	mov	edi, [disp_pos]
+	mov	ah, 0Fh
+.1:
+	lodsb
+	test	al, al
+	jz	.2
+	cmp	al, 0Ah	; 是回车吗?
+	jnz	.3
+	push	eax
+	mov	eax, edi
+	mov	bl, 160
+	div	bl
+	and	eax, 0FFh
+	inc	eax
+	mov	bl, 160
+	mul	bl
+	mov	edi, eax
+	pop	eax
+	jmp	.1
+.3:
+	mov	[gs:edi], ax
+	add	edi, 2
+	jmp	.1
+
+.2:
+	mov	[disp_pos], edi
+
+	pop	ebp
+	ret
+
+
+
+; ========================================================================
+;                  void disp_color_str(char * info, int color);
+; ========================================================================
+disp_color_str:
+	push	ebp
+	mov	ebp, esp
+
+	mov	esi, [ebp + 8]	; pszInfo
+	mov	edi, [disp_pos]
+	mov	ah, [ebp + 12]	; color
+.1:
+	lodsb
+	test	al, al
+	jz	.2
+	cmp	al, 0Ah	; 是回车吗?
+	jnz	.3
+	push	eax
+	mov	eax, edi
+	mov	bl, 160
+	div	bl
+	and	eax, 0FFh
+	inc	eax
+	mov	bl, 160
+	mul	bl
+	mov	edi, eax
+	pop	eax
+	jmp	.1
+.3:
+	mov	[gs:edi], ax
+	add	edi, 2
+	jmp	.1
+
+.2:
+	mov	[disp_pos], edi
+
+	pop	ebp
+	ret
+
+; ------------------------------------------------------------------------
+; 显示 AL 中的数字
+; ------------------------------------------------------------------------
+disp_al:
+	push	ecx
+	push	edx
+	push	edi
+
+	mov	edi, [disp_pos]
+
+	mov	ah, 0Fh			; 0000b: 黑底    1111b: 白字
+	mov	dl, al
+	shr	al, 4
+	mov	ecx, 2
+.begin:
+	and	al, 01111b
+	cmp	al, 9
+	ja	.1
+	add	al, '0'
+	jmp	.2
+.1:
+	sub	al, 0Ah
+	add	al, 'A'
+.2:
+	mov	[gs:edi], ax
+	add	edi, 2
+
+	mov	al, dl
+	loop	.begin
+	;add	edi, 2
+
+	mov	[disp_pos], edi
+
+	pop	edi
+	pop	edx
+	pop	ecx
+
+	ret
+; DispAL 结束-------------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; 显示一个整形数
+; ------------------------------------------------------------------------
+disp_int:
+	mov	eax, [esp + 4]
+	shr	eax, 24
+	call	disp_al
+
+	mov	eax, [esp + 4]
+	shr	eax, 16
+	call	disp_al
+
+	mov	eax, [esp + 4]
+	shr	eax, 8
+	call	disp_al
+
+	mov	eax, [esp + 4]
+	call	disp_al
+
+	mov	ah, 07h			; 0000b: 黑底    0111b: 灰字
+	mov	al, 'h'
+	push	edi
+	mov	edi, [disp_pos]
+	mov	[gs:edi], ax
+	add	edi, 4
+	mov	[disp_pos], edi
+	pop	edi
+
+	ret
+; DispInt 结束------------------------------------------------------------
+
+
+; ========================================================================
+;                  void out_byte(u16 port, u8 value);
+; ========================================================================
+out_byte:
+	mov	edx, [esp + 4]		; port
+	mov	al, [esp + 4 + 4]	; value
+	out	dx, al
+	nop	; 一点延迟
+	nop
+	ret
+
+; ========================================================================
+;                  u8 in_byte(u16 port);
+; ========================================================================
+in_byte:
+	mov	edx, [esp + 4]		; port
+	xor	eax, eax
+	in	al, dx
+	nop	; 一点延迟
+	nop
+	ret
+
