e19a6a0b1ce6c8e9fd087786e532ae134c9f018a
touchChen <rstyh.t@gmail.com>
2019-11-20 09:31:02 +0800

    up

--- mp/kernel/global.c
+++ mp/kernel/global.c
@@ -13,8 +13,8 @@
 PUBLIC  TASK  task_table[NR_TASKS] = {{task_tty, 2000, 0, STACK_SIZE_TTY, "tty"}};
 
 PUBLIC  TASK  user_proc_table[NR_PROCS] = {{TestA, 10, 0, STACK_SIZE_TESTA, "TestA"},
-					     {TestB, 1100, 2, STACK_SIZE_TESTB, "TestB"},
-					     {TestC, 10, 1, STACK_SIZE_TESTC, "TestC"}};
+					     {TestB, 10, 0, STACK_SIZE_TESTB, "TestB"},
+					     {TestC, 10, 0, STACK_SIZE_TESTC, "TestC"}};
  
 PUBLIC	system_call  sys_call_table[NR_SYS_CALL] = {sys_get_ticks,
                                                     sys_write};
