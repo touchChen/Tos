d3e281cd82f9e3af5b101c5be32fc98e4f02c1f0
touchChen <rstyh.t@gmail.com>
2020-11-25 17:31:32 +0800

    up

--- pm/run.asm
+++ pm/run.asm
@@ -10,26 +10,26 @@ org	0100h
 [SECTION .gdt]
 ; GDT
 ;                              段基址,        段界限     ,       属性
-LABEL_GDT:	   Descriptor       0,                  0,       0           ; 空描述符
-LABEL_DESC_NORMAL: Descriptor       0,             0ffffh,       DA_DRW      ; Normal 描述符
-LABEL_DESC_DATA:   Descriptor       0,          DataLen-1,       DA_DRW + DA_DPL2      ; Data
-LABEL_DESC_CODE32: Descriptor       0,   SegCode32Len - 1,       DA_C + DA_32; 非一致代码段
-LABEL_DESC_CODE16: Descriptor       0,             0ffffh,       DA_C        ; 非一致代码段, 16
-LABEL_DESC_VIDEO:  Descriptor 0B8000h,             0ffffh,       DA_DRW	+ DA_DPL3     ; 显存首地址
-LABEL_DESC_STACK32:  Descriptor     0,         TopOfStack,       DA_DRWA+DA_32; Stack, 32位
-LABEL_DESC_LDT:    Descriptor       0,         LDTLen - 1,       DA_LDT	      ; LDT
-LABEL_DESC_CODE_DEST: Descriptor    0,   SegCodeDestLen-1,       DA_C + DA_32; 非一致代码段,32
+LABEL_GDT:	   Descriptor       	0,                  0,       0           		; 空描述符
+LABEL_DESC_NORMAL: Descriptor       0,             0ffffh,       DA_DRW      		; Normal 描述符
+LABEL_DESC_DATA:   Descriptor       0,          DataLen-1,       DA_DRW + DA_DPL2   ; Data
+LABEL_DESC_CODE32: Descriptor       0,   SegCode32Len - 1,       DA_C + DA_32		; 非一致代码段
+LABEL_DESC_CODE16: Descriptor       0,             0ffffh,       DA_C        		; 非一致代码段, 16
+LABEL_DESC_VIDEO:  Descriptor 0B8000h,             0ffffh,       DA_DRW	+ DA_DPL3	; 显存首地址
+LABEL_DESC_STACK32:  Descriptor     0,         TopOfStack,       DA_DRWA + DA_32	; Stack, 32位
+LABEL_DESC_LDT:    Descriptor       0,         LDTLen - 1,       DA_LDT				; LDT
+LABEL_DESC_CODE_DEST: Descriptor    0,   SegCodeDestLen-1,       DA_C + DA_32		; 非一致代码段,32
 
 LABEL_DESC_CODE_RING3: Descriptor   0,  SegCodeRing3Len-1,       DA_C + DA_32 + DA_DPL3
-LABEL_DESC_STACK3:     Descriptor   0,        TopOfStack3,       DA_DRWA+DA_32+DA_DPL3
-LABEL_DESC_TSS:        Descriptor   0,           TSSLen-1,       DA_386TSS	   ;TSS
+LABEL_DESC_STACK3:     Descriptor   0,        TopOfStack3,       DA_DRWA + DA_32 + DA_DPL3
+LABEL_DESC_TSS:        Descriptor   0,           TSSLen-1,       DA_386TSS	   		;TSS
 ; 门                           目标选择子,   偏移,    DCount,    属性
 LABEL_CALL_GATE:   Gate SelectorCodeDest,       0,         0,   DA_386CGate + DA_DPL3
 ; GDT 结束
 
-GdtLen		equ	$ - LABEL_GDT	; GDT长度
-GdtPtr		dw	GdtLen - 1	; GDT界限
-		dd	0		; GDT基地址
+GdtLen		equ		$ - LABEL_GDT	; GDT长度
+GdtPtr		dw		GdtLen - 1		; GDT界限
+			dd		0				; GDT基地址
 
 ; GDT 选择子
 SelectorNormal		equ	LABEL_DESC_NORMAL	- LABEL_GDT
@@ -38,12 +38,12 @@ SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
 SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
 SelectorCode16		equ	LABEL_DESC_CODE16	- LABEL_GDT
 SelectorStack32		equ	LABEL_DESC_STACK32	- LABEL_GDT
-SelectorLDT		equ	LABEL_DESC_LDT		- LABEL_GDT
+SelectorLDT			equ	LABEL_DESC_LDT		- LABEL_GDT
 SelectorCodeDest	equ	LABEL_DESC_CODE_DEST	- LABEL_GDT
 SelectorCodeRing3	equ	LABEL_DESC_CODE_RING3	- LABEL_GDT + SA_RPL3
 SelectorCallGate	equ	LABEL_CALL_GATE 	- LABEL_GDT + SA_RPL3
 SelectorStack3		equ	LABEL_DESC_STACK3	- LABEL_GDT + SA_RPL3
-SelectorTSS		equ	LABEL_DESC_TSS		- LABEL_GDT
+SelectorTSS			equ	LABEL_DESC_TSS		- LABEL_GDT
 ; END of [SECTION .gdt]
 
 
@@ -53,9 +53,9 @@ ALIGN	32
 LABEL_DATA:
 SPValueInRealMode	dw	0
 ; 字符串
-PMMessage:		db	"Hello world, This is in Protect Mode now. ^-^", 0	; 在保护模式中显示
-OffsetPMMessage		equ	PMMessage - $$
-DataLen			equ	$ - LABEL_DATA
+PMMessage:			db		"Hello world, This is in Protect Mode now. ^-^", 0	; 在保护模式中显示
+OffsetPMMessage		equ		PMMessage - $$
+DataLen				equ		$ - LABEL_DATA
 ; END of [SECTION .data]
 
 ; 全局堆栈段
@@ -76,6 +76,7 @@ ALIGN	32
 [BITS	32]
 LABEL_STACK3:
 	times 512 db 0
+
 TopOfStack3	equ	$ - LABEL_STACK3 - 1
 ; END of [SECTION .s3]
 
@@ -86,8 +87,8 @@ ALIGN	32
 [BITS	32]
 LABEL_TSS:
 		DD	0			; Back
-		DD	TopOfStack		; 0 级堆栈
-		DD	SelectorStack32		; 
+		DD	TopOfStack			; 0 级堆栈
+		DD	SelectorStack32		; 0 级堆栈描述符
 		DD	0			; 1 级堆栈
 		DD	0			; 
 		DD	0			; 2 级堆栈
@@ -112,7 +113,7 @@ LABEL_TSS:
 		DD	0			; LDT
 		DW	0			; 调试陷阱标志
 		DW	$ - LABEL_TSS + 2	; I/O位图基址
-		DB	0ffh			; I/O位图结束标志
+		DB	0ffh				; I/O位图结束标志
 TSSLen		equ	$ - LABEL_TSS
 ; TSS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -122,54 +123,53 @@ TSSLen		equ	$ - LABEL_TSS
 [SECTION .s16]
 [BITS	16]
 LABEL_BEGIN:
-	mov	ax, cs
-	mov	ds, ax
-	mov	es, ax
-	mov	ss, ax
-	mov	sp, 0100h
+	mov		ax, cs
+	mov		ds, ax
+	mov		es, ax
+	mov		ss, ax
+	mov		sp, 0100h
 
-	mov	[LABEL_GO_BACK_TO_REAL+3], ax
-        mov	[SPValueInRealMode], sp
+	mov		[LABEL_GO_BACK_TO_REAL+3], ax
+	mov		[SPValueInRealMode], sp
  
-        call    DispStr
-
-        mov     di, (80 * 2 + 0) * 2
-        push    cs
-        call    DispInt
-        ;add     sp, 2         
+	call    DispStr
+	mov     di, (80 * 3 + 0) * 2
+	push    cs
+	call    DispInt
+         
 
 
 	; 初始化 16 位代码段描述符
-	mov	ax, cs
+	mov		ax, cs
 	movzx	eax, ax
-	shl	eax, 4
-	add	eax, LABEL_SEG_CODE16
-	mov	word [LABEL_DESC_CODE16 + 2], ax
-	shr	eax, 16
-	mov	byte [LABEL_DESC_CODE16 + 4], al
-	mov	byte [LABEL_DESC_CODE16 + 7], ah
+	shl		eax, 4
+	add		eax, LABEL_SEG_CODE16
+	mov		word [LABEL_DESC_CODE16 + 2], ax
+	shr		eax, 16
+	mov		byte [LABEL_DESC_CODE16 + 4], al
+	mov		byte [LABEL_DESC_CODE16 + 7], ah
 
 	; 初始化 32 位代码段描述符
-	xor	eax, eax
-	mov	ax, cs
-	shl	eax, 4
-	add	eax, LABEL_SEG_CODE32
-	mov	word [LABEL_DESC_CODE32 + 2], ax
-	shr	eax, 16
-	mov	byte [LABEL_DESC_CODE32 + 4], al
-	mov	byte [LABEL_DESC_CODE32 + 7], ah
+	xor		eax, eax
+	mov		ax, cs
+	shl		eax, 4
+	add		eax, LABEL_SEG_CODE32
+	mov		word [LABEL_DESC_CODE32 + 2], ax
+	shr		eax, 16
+	mov		byte [LABEL_DESC_CODE32 + 4], al
+	mov		byte [LABEL_DESC_CODE32 + 7], ah
 
-        ; 初始化数据段描述符
-	xor	eax, eax
-	mov	ax, ds
-	shl	eax, 4
-	add	eax, LABEL_DATA
-	mov	word [LABEL_DESC_DATA + 2], ax
-	shr	eax, 16
-	mov	byte [LABEL_DESC_DATA + 4], al
-	mov	byte [LABEL_DESC_DATA + 7], ah
+	; 初始化数据段描述符
+	xor		eax, eax
+	mov		ax, ds
+	shl		eax, 4
+	add		eax, LABEL_DATA
+	mov		word [LABEL_DESC_DATA + 2], ax
+	shr		eax, 16
+	mov		byte [LABEL_DESC_DATA + 4], al
+	mov		byte [LABEL_DESC_DATA + 7], ah
 
-        ; 初始化堆栈段描述符
+	; 初始化堆栈段描述符
 	xor	eax, eax
 	mov	ax, ds
 	shl	eax, 4
@@ -178,9 +178,8 @@ LABEL_BEGIN:
 	shr	eax, 16
 	mov	byte [LABEL_DESC_STACK32 + 4], al
 	mov	byte [LABEL_DESC_STACK32 + 7], ah
-
         
-        ; 初始化堆栈段描述符(STACK3)
+	; 初始化堆栈段描述符(STACK3)
 	xor	eax, eax
 	mov	ax, ds
 	shl	eax, 4
@@ -281,22 +280,22 @@ LABEL_BEGIN:
 
 	; 真正进入保护模式
 	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs,
-					; 并跳转到 Code32Selector:0  处
+								; 并跳转到 Code32Selector:0  处
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 DispStr:
-	mov	ax, BootMessage
-	mov	bp, ax			; ES:BP = 串地址
-	mov	cx, lenMessage		; CX = 串长度
-	mov	ax, 01301h		; AH = 13,  AL = 01h
-	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
-	mov	dl, 0
-	int	10h			; 10h 号中断
+	mov		ax, BootMessage
+	mov		bp, ax				; ES:BP = 串地址
+	mov		cx, lenMessage		; CX = 串长度
+	mov		ax, 01301h			; AH = 13,  AL = 01h
+	mov		bx, 000ch			; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov		dl, 0
+	int		10h					; 10h 号中断
 	ret
 
-BootMessage:		db	"Hello, this in real model!"
-lenMessage:             equ     $ - BootMessage
+BootMessage:		db		"Hello, this in real model!"
+lenMessage:			equ		$ - BootMessage
 
 
 
@@ -337,17 +336,17 @@ DISPLAYSEG	equ 0xb800
 
 
 DispInt:
-        mov     bx,  sp      ; 当前sp指向压入的ip(返回后的下条指令)
-        add     bx,  2
+	mov		bx,  sp      ; 当前sp指向压入的ip(返回后的下条指令)
+	add		bx,  2
 
-        mov     ax, [ss:bx]       
-	shr	ax, 8
+	mov		ax, [ss:bx]       
+	shr		ax, 8
 	call	DispALReal
 
-        mov     ax, [ss:bx]
-        call	DispALReal
+	mov     ax, [ss:bx]
+	call	DispALReal
 
-        ret     2  
+	ret     2  
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -369,6 +368,8 @@ LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
 	int	21h		; /  回到 DOS
 
 ; END of [SECTION .s16]
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;保护模式
